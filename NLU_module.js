7352cb7adadc3137f9d7ead4a9a83df373e1fd85
Ahmed Youssef <ahmedalaa@windowslive.com>
2017-06-19 08:56:51 -0400

    Update NLU_module.js

--- NLU_module.js
+++ NLU_module.js
@@ -1,15 +1,15 @@
 //setting up NLU 
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-  'username': 'enter your IBM bluemix username',
-  'password': 'enter your bluemix password',
+  'username': '0025daef-697a-405f-b544-62b39830f9a8',
+  'password': 'KP6iG4kZmKti',
   'version_date': '2017-02-27'
 });
 
   //only one function in this module
   module.exports = {
 
-  analyze: function(text){
+  analyze: function(text, callback){
 
   var parameters = {
   	'text':text,
@@ -31,7 +31,7 @@ var natural_language_understanding = new NaturalLanguageUnderstandingV1({
   if (err)
     console.log('error:', err);
   else
-	return JSON.stringify(response,null,2);
+	callback(response);
 });
 }
 
