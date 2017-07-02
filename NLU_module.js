54378a16a5ed4963d61f4c1c3e1a513fe4a8fe98
Ahmed Youssef <ahmedalaa@windowslive.com>
2017-07-02 18:29:47 -0400

    need new credentials

--- NLU_module.js
+++ NLU_module.js
@@ -6,14 +6,18 @@ var natural_language_understanding = new NaturalLanguageUnderstandingV1({
   'version_date': '2017-02-27'
 });
 
-  //only one function in this module
   module.exports = {
 
+  //returns analysis with default features
   analyze: function(text, callback){
 
   var parameters = {
   	'text':text,
     'features':{
+      'categories': {},
+      'concepts': {
+      'limit': 5
+      },
       'entities':{
         'emotion': true,
         'sentiment': true,
@@ -26,6 +30,40 @@ var natural_language_understanding = new NaturalLanguageUnderstandingV1({
       }
     }
   }
+
+  natural_language_understanding.analyze(parameters, function(err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+	callback(response);
+});
+},
+
+//returns analysis with only the passed features
+analyze: function(text, features, callback){
+
+  var parameters = {
+  	'text':text,
+    'features':{}
+  }
+  if(features.includes('categories'))
+    parameters.features.categories = {};
+  if(features.includes('concepts'))
+      parameters.features.concepts = {
+      'limit': 5
+      };
+  if(features.includes('entities'))
+      parameters.features.entities = {
+        'emotion': true,
+        'sentiment': true,
+        'limit': 10
+      };
+   if(features.includes('keywords'))
+      parameters.features.keywords = {
+        'emotion':true,
+        'sentiment':true,
+        'limit':10
+      };
 
   natural_language_understanding.analyze(parameters, function(err, response) {
   if (err)
